296aa7216425da7b7b7f422df3b8f3981cb62d91
Iftodii Mihai <Mihai.Iftodii@endava.com>
2016-03-11 15:51:29 +0200

    Improved class Students

--- Students.java
+++ Students.java
@@ -1,40 +1,69 @@
-public class Students {	
-	String firstname;
-	String lastname;
-	String speciality;
-	int nrExam;
-	double[] notes;
+public class Students {
 	
-	public Students(){
-		firstname = "Mihai";
-		lastname = "Iftodii";
-		nrExam = 9;
-		speciality = "Applied Informatics";
-		for(int i=0; i<nrExam; i+=3 ){
-			notes[i] = 9;
-			notes[i+1] = 8;
-			notes[i+2] =10;
-		}
-	}	
-	public void set_firstname(String new_firstname){
-		firstname = new_firstname;
+	private String firstname;
+	private String lastname;
+	private String speciality;
+	private int nrExam;
+	private int notes[];
+	
+public Students(){
+	firstname = "Mihai";
+	lastname = "Iftodii";
+	nrExam = 9;
+	speciality = "Applied Informatics";
+	notes = new int[nrExam];
+	for(int i=0; i<nrExam; i+=3 ){
+	notes[i] = 9;
+	notes[i+1] = 8;
+	notes[i+2] =10;
 	}
-	public String get_firstname(){
-		return firstname;
+}	
+public void setFirstname(String firstname){
+	this.firstname = firstname;
+}
+public String getFirstname(){
+	return firstname;
+}
+public void setLastname(String lastname){
+	this.lastname = lastname;
+}
+public String getLastname(){
+	return lastname;
+}
+public void setSpeciality(String speciality){
+	this.speciality = speciality;
+}
+public String getSpeciality(){
+	return speciality;
+}
+public void setNrExam(int nrExam){
+	if((nrExam > 0) && (nrExam < 9) && (nrExam != this.nrExam)){
+	int tmp[]= new int [this.nrExam];
+	for(int i=0; i<this.nrExam; i++)
+	tmp[i] = notes[i];
+	notes= new int[nrExam];
+	if(nrExam>this.nrExam)
+	for(int i=0; i<this.nrExam;i++)
+	notes[i]=tmp[i];
+	else
+	for(int i=0; i<nrExam;i++)
+	notes[i]=tmp[i];
+	this.nrExam = nrExam;
 	}
-	public void printInfo(){
-		System.out.println("Firstname: " + firstname);
-		System.out.println("Lastname: " + lastname);
-		System.out.println("Speciality: " + speciality);
-		for(int i=0; i<nrExam; i++){
-			System.out.println("Note: " + notes[i]);
-		}
+}
+public int getNrExam(){
+	return nrExam;
+}
+public void setNotes(int index, int notes){
+	if((index >= 0) && (index < nrExam)){
+	this.notes[index] = notes;
 	}
-	public static void main(String[] args) {
-		Students student1 = new Students();
-		student1.printInfo();
-		student1.get_firstname();
-		student1.set_firstname("Alexandru");
-		student1.printInfo();
+}
+public int getNotes(int index){
+	if((index >= 0) && (index < nrExam)){
+	return notes[index];
 	}
+	else
+	return -1;	
+}
 }
