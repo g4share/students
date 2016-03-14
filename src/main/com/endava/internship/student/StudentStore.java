e79441458c8144dc5fb54135684cd983fa36cfbb
Iftodii Mihai <Mihai.Iftodii@endava.com>
2016-03-14 17:26:04 +0200

    Clean trash and modify java version to 1.7

--- src/main/com/endava/internship/student/StudentStore.java
+++
@@ -1,9 +0,0 @@
-package com.endava.internship.student;
-
-import java.util.List;
-
-public interface StudentStore {
-	List<Student> getStudents();
-	void add(Student student);	
-	void remove(Student student);
-}
