5fe8ee128b24abb5add95b3108021c23e7f8548d
Iftodii Mihai <Mihai.Iftodii@endava.com>
2016-03-15 16:56:07 +0200

    Return students in java servlet and junit tests

--- src/test/java/com/endava/internship/service/PrintServiceTest.java
+++ src/test/java/com/endava/internship/service/PrintServiceTest.java
@@ -12,21 +12,22 @@ import com.endava.internship.student.StudentStorage;
 
 public class PrintServiceTest {
 	private StudentStorage storage = new StudentStorage();
-	private static String v = "<html><head><title>TableServlet</title><link href=styles.css rel=stylesheet type=text/css></head><body><table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Speciality</th></tr><tr><td>1</td><td>Bob</td><td>Mackenzie</td><td>IA</td></tr><tr><td>2</td><td>Charlie</td><td>Weasley</td><td>MA</td></tr><tr><td>3</td><td>Dean</td><td>Thomas</td><td>MnI</td></tr></table></body></html>";
+	private List<Student> students = new ArrayList<>();
+	private static String v1 = "<html><head><title>TableServlet</title><link href=styles.css rel=stylesheet type=text/css></head><body><table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Speciality</th></tr><tr><td>1</td><td>Bob</td><td>Mackenzie</td><td>IA</td></tr><tr><td>2</td><td>Charlie</td><td>Weasley</td><td>MA</td></tr><tr><td>3</td><td>Dean</td><td>Thomas</td><td>MnI</td></tr></table></body></html>";
+	private static String v2 = "<html><head><title>TableServlet</title><link href=styles.css rel=stylesheet type=text/css></head><body>No student found</body></html>";
+
 	@Test
 	public void getHtmlTest(){
-		List<Student> students = new ArrayList<>();
 		students = storage.getStudents();
 		PrintService printservice = new PrintService();
 		String html = printservice.getHtml(students);
-		assertEquals(html, v);
+		assertEquals(html, v1);
 	}
 	
 	@Test
 	public void getHtmlTest1(){
 		PrintService printservice = new PrintService();
 		String html = printservice.getHtml(new ArrayList<Student>());
-		String t = "<html><head><title>TableServlet</title><link href=styles.css rel=stylesheet type=text/css></head><body>No student found</body></html>";
-		assertEquals(html, t);
+		assertEquals(html, v2);
 	}
 }
