d1164c1844c95a600cc4c618a0458e79789e0391
Iftodii Mihai <Mihai.Iftodii@endava.com>
2016-03-16 10:42:50 +0200

    Alignment and jococo tags in pom

--- src/main/java/com/endava/internship/service/PrintService.java
+++ src/main/java/com/endava/internship/service/PrintService.java
@@ -8,7 +8,6 @@ public class PrintService {
 	
 	public String getHtml(List<Student> students){
 		StringBuilder sb = new StringBuilder();
-
 		sb.append("<html><head><title>TableServlet</title><link href=styles.css rel=stylesheet type=text/css></head><body>");
 		addStudent(sb, students);
 		sb.append("</body></html>");
@@ -23,13 +22,13 @@ public class PrintService {
 		sb.append("<table><tr>")
 			.append("<th>ID</th><th>First Name</th><th>Last Name</th><th>Speciality</th>")
 			.append("</tr>");
-	for(Student st : students){
-		sb.append("<tr><td>")
-			.append(st.getId() + "</td><td>")
-			.append(st.getFirstName() + "</td><td>")
-			.append(st.getLastName() + "</td><td>") 
-			.append(st.getSpeciality() + "</td></tr>");
-	}
-	sb.append("</table>");
+		for(Student st : students){
+			sb.append("<tr><td>")
+				.append(st.getId() + "</td><td>")
+				.append(st.getFirstName() + "</td><td>")
+				.append(st.getLastName() + "</td><td>") 
+				.append(st.getSpeciality() + "</td></tr>");
+		}
+		sb.append("</table>");
 	}
 }
